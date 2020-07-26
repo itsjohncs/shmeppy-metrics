051e92cfaa022f121f51d24058282628c9870d47
itsjohncs <johnsullivan.pem@gmail.com>
2020-07-25 17:18:23 -0700

    Added datetime parsing.

--- src/fast-convoker/src/parse.rs
+++ src/fast-convoker/src/parse.rs
@@ -45,52 +45,106 @@ pub fn parse_uuid(raw: &[u8]) -> Option<UUID> {
 }
 
 
+pub fn parse_datetime(raw: &[u8]) -> Option<chrono::DateTime<chrono::Utc>> {
+    use chrono::offset::TimeZone;
+    chrono::Utc.datetime_from_str(
+        // The input is fairly trusted, so if this proves to be slow (and I've
+        // seen utf8 decoding be _very_ slow) I can probably get away with
+        // an unchecked call, or doing a quick "is this lower-ascii like I
+        // expect?" and then doing a safe unchecked call.
+        std::str::from_utf8(raw).ok()?,
+        "%-m/%-d/%Y %-I:%M:%S %p").ok() // ok? ok
+}
+
+
 #[cfg(test)]
 mod tests {
-    use crate::parse::{UUID, parse_uuid};
+    mod timestamp {
+        use chrono::Utc;
+        use chrono::offset::TimeZone;
+        use crate::parse::parse_datetime;
 
-    #[test]
-    fn standard_format() {
-        let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed9c";
-        let uuid = parse_uuid(raw_uuid);
-        assert!(uuid.is_some());
+        #[test]
+        fn standard_format() {
+            let raw_datetime = b"4/25/2020 9:55:14 PM";
+            let datetime = parse_datetime(raw_datetime);
+            assert!(datetime.is_some());
+            assert_eq!(datetime.unwrap(),
+                       Utc.ymd(2020, 4, 25).and_hms(21, 55, 14));
+        }
 
-        assert_eq!(uuid.unwrap(),
-                   UUID(152440156471620327380106519705898118556));
+        #[test]
+        fn missing_am_pm() {
+            let raw_datetime = b"4/25/2020 9:55:14";
+            let datetime = parse_datetime(raw_datetime);
+            assert!(datetime.is_none());
+        }
+
+        #[test]
+        fn empty() {
+            let raw_datetime = b"";
+            let datetime = parse_datetime(raw_datetime);
+            assert!(datetime.is_none());
+        }
+
+        #[test]
+        fn bad_utf8() {
+            // There's no way to tell whether the UTF8 decoder is correctly
+            // failing or whether it fails at the parse step when it realizes
+            // this is most certainly not a date. Downside of using Option as
+            // a carrier of the error status.
+            let raw_datetime = b"\xc3\x28";
+            let datetime = parse_datetime(raw_datetime);
+            assert!(datetime.is_none());
+        }
     }
 
-    #[test]
-    fn missing_dashes() {
-        let raw_uuid = b"72aee7fd384249cfa196be874a72ed9c";
-        let uuid = parse_uuid(raw_uuid);
-        assert!(uuid.is_some());
+    mod uuid {
+        use crate::parse::{UUID, parse_uuid};
 
-        assert_eq!(uuid.unwrap(),
-                   UUID(152440156471620327380106519705898118556));
-    }
+        #[test]
+        fn standard_format() {
+            let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed9c";
+            let uuid = parse_uuid(raw_uuid);
+            assert!(uuid.is_some());
 
-    #[test]
-    fn empty() {
-        let raw_uuid = b"";
-        assert!(parse_uuid(raw_uuid).is_none());
-    }
+            assert_eq!(uuid.unwrap(),
+                       UUID(152440156471620327380106519705898118556));
+        }
 
-    #[test]
-    fn non_dashes() {
-        let raw_uuid = b"72aee7fd_3842-49cf-a196-be874a72ed9c";
-        assert!(parse_uuid(raw_uuid).is_none());
-    }
+        #[test]
+        fn missing_dashes() {
+            let raw_uuid = b"72aee7fd384249cfa196be874a72ed9c";
+            let uuid = parse_uuid(raw_uuid);
+            assert!(uuid.is_some());
 
-    #[test]
-    fn truncated() {
-        let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed";
-        assert!(parse_uuid(raw_uuid).is_none());
-    }
+            assert_eq!(uuid.unwrap(),
+                       UUID(152440156471620327380106519705898118556));
+        }
 
-    #[test]
-    fn too_long() {
-        let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed9c\
-                         72aee7fd-3842-49cf-a196-be874a72ed9c";
-        assert!(parse_uuid(raw_uuid).is_none());
+        #[test]
+        fn empty() {
+            let raw_uuid = b"";
+            assert!(parse_uuid(raw_uuid).is_none());
+        }
+
+        #[test]
+        fn non_dashes() {
+            let raw_uuid = b"72aee7fd_3842-49cf-a196-be874a72ed9c";
+            assert!(parse_uuid(raw_uuid).is_none());
+        }
+
+        #[test]
+        fn truncated() {
+            let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed";
+            assert!(parse_uuid(raw_uuid).is_none());
+        }
+
+        #[test]
+        fn too_long() {
+            let raw_uuid = b"72aee7fd-3842-49cf-a196-be874a72ed9c\
+                             72aee7fd-3842-49cf-a196-be874a72ed9c";
+            assert!(parse_uuid(raw_uuid).is_none());
+        }
     }
 }
